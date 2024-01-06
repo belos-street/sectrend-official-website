2e3b96d65d59a2b8e7865c7f33daf3adc21570a7
aa1213889 <869135421@qq.com>
2024-01-06 22:05:52 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd'
+import { LayoutFooter } from '@/layouts/ly-footer'
+
+export default function Home() {
+  return (
+    <main>
+      <Button type="primary">Button</Button>
+      <LayoutFooter />
+    </main>
+  )
+}
---
+++ src/layouts/ly-footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.sass'
+
+export function LayoutFooter() {
+  return (
+    <footer className="layout-footer">
+      <section className="screen-center">233</section>
+      <section className="screen-center copyright">
+        <p>版权所有 © 上海安势信息技术有限公司</p>
+        <p>沪ICP备2021017773号</p>
+      </section>
+    </footer>
+  )
+}
